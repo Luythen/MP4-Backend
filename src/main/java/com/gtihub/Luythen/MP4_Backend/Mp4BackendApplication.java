65dcf41ca299dd54b5a86c200b3b649fc585aa50
Luythen <andenklasson@gmail.com>
2026-08-31 11:51:01 +0200

    Init

---
+++ src/main/java/com/gtihub/Luythen/MP4_Backend/Mp4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.gtihub.Luythen.MP4_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtihub/Luythen/MP4_Backend/Mp4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtihub.Luythen.MP4_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
