03189b4b0c741162efb11fdaec15b9790f9e482e
Tibbe <tlin.jonas@gmail.com>
2026-09-10 09:32:27 +0200

    Updated QuestionModel constructor, Rewritten QuestionService to use the mongoDB database. Imported questions from JSON. a document count serves as the base for random. added jackson-databind to pom

--- src/main/java/com/gtihub/Luythen/MP4_Backend/Question/QuestionModel.java
+++ src/main/java/com/gtihub/Luythen/MP4_Backend/Question/QuestionModel.java
@@ -2,18 +2,33 @@ package com.gtihub.Luythen.MP4_Backend.Question;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document (collection = "questions")
 public class QuestionModel {
 
-    private String id;
+    @Id
+    private int id;
     private String question;
     private String category;
     private List<String> options;
     private String correctAnswer;
 
-    public String getId() {
+    public QuestionModel() {
+    }
+
+    public QuestionModel(int id, String question, String category, List<String> options, String correctAnswer) {
+        this.id = id;
+        this.question = question;
+        this.category = category;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+    public int getId() {
         return id;
     }
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
     public String getQuestion() {
